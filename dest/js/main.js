afec9a9a345e5cdc2d9f4e6d9f4b0584875aa89f
artklim <artklim777@gmail.com>
2018-03-14 18:26:33 +0300

    2 commit

--- dest/js/main.js
+++ dest/js/main.js
@@ -1,7 +1,27 @@
-
- $(document).ready(function(){
-      $('.bxslider').bxSlider({
- slideWidth: 600,
- pager: false
+$('.bxslider').bxSlider({
+    slideWidth: 600,
+    pager: false
 });
-    });
+
+
+
+
+
+
+
+
+
+
+/* Fixed navigation */
+	var stickyOffset = $('#navigation').offset().top + 1;
+	$(window).scroll(function() {
+
+		if ($(window).scrollTop() >= stickyOffset) {
+			$('#navigation').addClass('fixed');
+			$('#about').css('margin-top', navbarHeight);
+		} else {
+			$('#navigation').removeClass('fixed');
+			$('#about').css('margin-top', 0);
+		}
+	});
+	/* END fixed navigation */
